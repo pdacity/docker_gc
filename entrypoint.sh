2df7ca6982c6bb9c0006625cb751ba743b0aef90
Dmitry Malinin <pdacity@ya.ru>
2020-09-10 20:55:08 +0000

    some cosmetic changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 while true; do
-    echo "🔔   Docker was pruned everything older than $AGE and unused volumes !"
+    echo "🔔  Docker was pruned everything older than $AGE and unused volumes !"
         docker system prune --force --all --filter until=$AGE --filter label!=$STICKY_LABEL 
         docker volume prune --force  
     echo
     if [ -n "$SLEEP" ]; then
-        echo "💤   Now sleep, next check in $SLEEP ..."
+        echo "💤  Now sleep, next check in $SLEEP ..."
         sleep "$SLEEP"
         echo
     else
